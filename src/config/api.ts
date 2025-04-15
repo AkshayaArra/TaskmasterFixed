af18dfdac87cfd4fa331aa602fa4752c194b844a
akshaya <akshayaa.btech22@rvu.edu.in>
2025-04-15 15:24:11 +0530

    api.ts fixed

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 
 // API configuration
-const API_URL = import.meta.env.VITE_API_URL || "https://taskmaster-backend-service.onrender.com";
+const API_URL = import.meta.env.VITE_API_URL || "https://taskmaster-3-41fr.onrender.com";
 
 export default API_URL;
